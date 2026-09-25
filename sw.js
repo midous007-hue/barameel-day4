12a0ee21b98af8eeb06006c5432b30f4928afd2d
midous007-hue <midous007@gmail.com>
2026-09-25 16:15:52 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='barameel-run-new-v4';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./screen02.html','./screen03.html','./screen04.html','./screen05.html','./screen06.html','./app.js','./styles.css','./audio/reward-levelup.mp3'])))});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
