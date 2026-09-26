6b41e4a845e90f75130417c98b2144b8e2ca62fa
midous007-hue <midous007@gmail.com>
2026-09-26 15:13:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE="barameel-run-alexandria-v24";
+const CACHE="barameel-run-alexandria-v25";
 const SHELL=[
   "./","./index.html","./screen02.html","./screen03.html","./screen04.html","./screen05.html","./screen06.html",
   "./app.js","./styles.css","./sw.js","./assets/screen01-start.webp","./assets/screen01-start-mobile.webp",
   "./assets/screen05-scanner.webp","./assets/screen06-puzzle.webp","./assets/screen06-puzzle-mobile.webp",
-  "./assets/collections/collection01/collection.json"
+  "./assets/collections/collection01/collection.json","./assets/collections/collection01/thumbs/image01.webp","./assets/collections/collection01/thumbs/image02.webp","./assets/collections/collection01/thumbs/image03.webp","./assets/collections/collection01/thumbs/image04.webp","./assets/collections/collection01/thumbs/image05.webp","./assets/collections/collection01/thumbs/image06.webp","./assets/collections/collection01/thumbs/image07.webp","./assets/collections/collection01/thumbs/image08.webp","./assets/collections/collection01/thumbs/image09.webp","./assets/collections/collection01/thumbs/image10.webp"
 ];
 const isStaticAsset=(url)=>/\.(?:webp|png|jpg|jpeg|css|js|json|mp3)$/i.test(url.pathname);
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
